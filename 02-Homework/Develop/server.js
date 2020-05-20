4b933cd9e153beb68525ed7e1d991d9c98bb2c8f
sidseal <sidseal@me.com>
2020-05-20 01:09:28 -0400

    Server.js added

---
+++ 02-Homework/Develop/server.js
@@ -0,0 +1,25 @@
+//Dependencies//
+const express = require("express");
+
+//Express
+const app = express();
+
+//Port
+const PORT = process.env.PORT || 8080;
+
+//Express to data parse to JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+
+
