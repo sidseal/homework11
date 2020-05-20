a5b45726efc98c6fdecfc172735b27acd7b4bc69
sidseal <sidseal@me.com>
2020-05-20 01:35:45 -0400

    htmlroute added

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 //Port
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 //Express to data parse to JSON
 app.use(express.urlencoded({ extended: true }));
