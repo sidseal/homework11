1180ac276499e82da6c0c2a4c341a378c3bfcafb
sidseal <sidseal@me.com>
2020-05-22 20:21:33 -0400

    working on delete

--- 02-Homework/Develop/routes/apiRoutes.js
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -13,44 +13,65 @@ module.exports = function (app) {
 
     app.get("/api/notes", function (req, res) {
         fs.readFile("db/db.json", "utf8", function (err, data) {
-            // if (err) throw err;
-            console.log(data)
+            if (err) throw err;
+            // console.log(data)
              res.json(JSON.parse(data));
+            //  notes = JSON.parse(data)
         })
     })
 
     app.post("/api/notes", function (req, res) {
-        fs.readFile("db/db.json", "utf8", function (err, data) {
-            // if (err) throw err;
-            const array = JSON.parse(data)
+        // fs.readFile("db/db.json", "utf8", function (err, data) {
+        //     if (err) throw err;
+            // const array = JSON.parse(data)
             const newNote = {
                 title: req.body.title,
                 text: req.body.text,
                 id: shortid.generate(),
             };
-            array.push(newNote)
+            notes.push(newNote)
+            res.json(notes)
             //console.log(newNote.id);
-            fs.writeFile("db/db.json", JSON.stringify(array), function (err2) {
-                if (err2) throw err2;
+            fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+                if (err) throw err;
             });
-                res.json(newNote);
+                // res.json(newNote);
            
         })
 
-    })
-    app.delete('/api/notes/:id', function (req, res) {
-        const deleteID = req.params.id
-        fs.readFile("db/db.json", "utf8", function (err, data) {
-            if (err) throw err;
-            const array = JSON.parse(data)
-            const index = array.findIndex(ob => ob.id == deleteID)
-            console.log(index)
-            array.splice(index, 1)
-            fs.writeFile("db/db.json", JSON.stringify(array), function (err2, data2) {
-                if (err2) throw err2;
-                res.json(JSON.stringify(data2));
-            });
-        })
-    })
+    }
 
-}
+    app.delete("/api/notes/:id", function(req, res) {
+
+        fs.readFile("db/db.json", "utf8", function(err, data){
+          if(err)throw err;
+    
+          const array = JSON.parse(data);
+          const newDataArray = array.filter(note => {
+            return note.id == req.params.id;
+          })
+    
+          fs.writeFile("db/db.json", JSON.stringify(newDataArray), function(err){
+            if(err)throw err;
+            
+            console.log("Your note was deleted")
+          })
+        });
+        res.end();
+      });
+    
+    // app.delete('/api/notes/:id', function (req, res) {
+    //     const deleteID = req.params.id
+    //     fs.readFile("db/db.json", "utf8", function (err, data) {
+    //         if (err) throw err;
+    //         const array = JSON.parse(data)
+    //         const index = array.findIndex(ob => ob.id == deleteID)
+    //         console.log(index)
+    //         array.splice(index, 1)
+    //         fs.writeFile("db/db.json", JSON.stringify(array), function (err2, data2) {
+    //             if (err2) throw err2;
+    //             res.json(JSON.stringify(data2));
+    //         });
+    //     })
+    // })
+
